addd32076ba615a83cdeb242ed90eca207540f74
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-04-06 13:58:40 -0500

    added code

--- hshell_printenv.c
+++ hshell_printenv.c
@@ -1,1 +1,18 @@
-prints envirement
+#include "main.h"
+
+/**
+* _print_env - prints the environment built-in
+* Return: nothing
+*/
+
+void _print_env(void)
+  
+{
+  int i = 0;
+  
+  while (environ[i])
+  {
+    printf("%s\n",environ[i]);
+    i++;
+  }
+}
